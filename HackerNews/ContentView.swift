9e7f94130310ecbf36f3c0a4585c4f833187c86b
daniel ortiz millan <danielortizmillan@MacBook-Air-de-daniel.local>
2024-04-03 17:33:47 -0600

    first commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by daniel ortiz millan on 03/04/24.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by daniel ortiz millan on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
